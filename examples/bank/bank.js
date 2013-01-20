8e9bdc7292b8b2ffbb8bbd6ffc8950cd2e35689b
Dick Hardt <dickhardt@gmail.com>
2013-01-20 14:18:23 -0800

    First major commit

--- examples/bank/bank.js
+++ examples/bank/bank.js
@@ -1,4 +1,4 @@
-/* 
+/*
 * Bank Server code
 *
 * Copyright (C) Province of British Columbia, 2013
@@ -12,7 +12,8 @@ exports.app = function() {
 		console.log(req.domain);
 		console.log(req.headers);
 	    html = 'Hello World, from the Bank!';
-	    res.send(html);    
+	    res.send(html);
 	});
 	return app
+
 }
